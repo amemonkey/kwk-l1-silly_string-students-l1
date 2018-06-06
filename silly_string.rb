b50c459cc267eb5fc5d0315e61a4160e9b966e8f
amemonkey <ameliafunaki789@gmail.com>
2018-06-06 20:36:54 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,22 +1,18 @@
 
 # Challenge 1: Fill this string in with your favorite word
-
 your_favorite_word = "magenta"
 puts "My favorite word is #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
-
 favorite_word_in_all_caps = your_favorite_word.upcase  
 puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
-
 phrase = "IM NOT SHOUTING"
 lowercased_phrase = phrase.downcase 
 puts lowercased_phrase
 
 # Challenge 4: Count how many letters are in that big_word
-
 big_word = "supercalifragilisticexpialidocious"
 letter_count = big_word.length 
 puts "There are #{letter_count} letters in #{big_word}."
@@ -26,11 +22,7 @@ bigger_word = "big_word/" + "wow" + "mom"
 new_letter_count = bigger_word.length
 puts "There are now #{new_letter_count} in #{bigger_word}"
 
-# source = "#{ROOT_DIR}/" + project + "/App.config"
-
-
 # Challenge 6: Capitalize this sentence properly
-
 proper_sentence = "i really like programming."
 fixed_sentence = proper_sentence.capitalize
 puts fixed_sentence
@@ -40,6 +32,6 @@ sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples 
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
-sentence = sentence.gsub # Add your gsub chain here
+sentence = sentence.gsub.chomp # Add your gsub chain here
 
-# puts sentence
+puts sentence

a760cad600da78a5b50e00b5e2df00bb596b6749
amemonkey <ameliafunaki789@gmail.com>
2018-06-06 20:34:37 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -22,10 +22,11 @@ letter_count = big_word.length
 puts "There are #{letter_count} letters in #{big_word}."
 
 # # Challenge 5: Add "wow" and "mom" to big_word
-bigger_word = "#{big_word}" + "wow" + "mom"
+bigger_word = "big_word/" + "wow" + "mom"
 new_letter_count = bigger_word.length
 puts "There are now #{new_letter_count} in #{bigger_word}"
 
+# source = "#{ROOT_DIR}/" + project + "/App.config"
 
 
 # Challenge 6: Capitalize this sentence properly

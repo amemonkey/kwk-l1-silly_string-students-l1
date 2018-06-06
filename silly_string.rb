594ec720ea15e8671d6c7b9b723a0834dd53dd2b
amemonkey <ameliafunaki789@gmail.com>
2018-06-06 20:19:00 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -22,8 +22,8 @@
 # puts "There are #{letter_count} letters in #{big_word}."
 
 # Challenge 5: Add "wow" and "mom" to big_word
-
-bigger_word = big_word + "wow" + "mom" 
+def bigger_word("wow", "mom")
+bigger_word = big_word
 new_letter_count = bigger_word.size.length
 puts "There are now #{new_letter_count} in #{bigger_word}"
 
